1c8138b52be2316fb5525ac8098d315f9173c095
Luca Gibelli <nervous@tzone.nervous.it>
2018-05-21 20:34:04 +0200

    update navigation depth for sidebar

--- conf.py
+++ conf.py
@@ -104,6 +104,9 @@ html_theme = 'default'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'navigation_depth': 4,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
